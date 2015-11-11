d3387d2f633473352345e0e505e2b45a18b25c26
陈锐 <chenrui_accp@163.com>
2015-11-11 19:49:33 +0800

    修改注释

--- test001.py
+++ test001.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-  
-# one day console in & console out
-print 'console out 中文'
+# one day console in & console out 控制台输入和输出练习
+print 'console out'
 print 'console in(have not agrs):'
 consoleIn = raw_input()
 print 'you set:' + consoleIn + 'to console'
