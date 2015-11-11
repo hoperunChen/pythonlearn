4a34d9f58aae252d1694865d13b25d5f1049d97e
陈锐 <chenrui_accp@163.com>
2015-11-11 19:45:04 +0800

    one

---
+++ test001.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-  
+# one day console in & console out
+print 'console out 中文'
+print 'console in(have not agrs):'
+consoleIn = raw_input()
+print 'you set:' + consoleIn + 'to console'
+print 'console in(have agrs):'
+consoleIn1 = raw_input('please set something:')
+print 'you set:' + consoleIn1
---
+++ test002.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-  
+#one day 条件语句练习001
+initNum = 39
+userNum = int(raw_input('please set a number:'))
+if userNum > initNum :
+	print 'big'
+else:
+	print 'small'
