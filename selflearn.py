f94fbb8ff957437c2178fab19bf372442d131a76
陈锐 <chenrui_accp@163.com>
2015-11-20 15:12:11 +0800

    函数的参数&练习

---
+++ selflearn.py
@@ -0,0 +1,91 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-  
+# 练习
+
+FGF = '************************'
+LINE = '\n'
+TAB = '\t'
+YES = 'Y'
+No = 'N'
+
+def _print(msg=''):
+	print _toUtf8(msg)
+
+def _toUtf8(msg):
+	return '%s'%(msg.decode('utf-8'))
+
+def main(args=None):
+	if(args!=None):
+		if not isinstance(args, dict):
+			raise TypeError("shoud set a dict args")
+	else:
+		uIn = helloPage()
+		if uIn == '1':
+			login()
+		elif uIn == '2':
+			response = register()
+			if response['code']==-1:
+				main()
+			else:
+				doLogin(response)
+
+
+
+def helloPage():
+	_print(FGF+'欢迎使用'+FGF)
+	_print('请选择:')
+	_print(TAB+'1.登录')
+	_print(TAB+'2.注册')
+	_print(TAB+'3.退出')
+	_print('请输入序号(回车提交):')
+	return raw_input()
+
+def login():
+	#TODO 
+	_print('开始登录')
+	pass
+
+def doLogin(request):
+	#TODO 
+	_print('doLogin:'+request['userName']+','+request['userPass'])
+	pass
+
+def register():
+	rtn = {'userName':None,'userPass':None,'code':-1}
+	_print(FGF+'注册'+FGF)
+	_print('请输入用户名:')
+	userName  = raw_input()
+	_print('请输入密码:')
+	userPass = raw_input()
+	_print('以下是您的输入信息请确认:')
+	_print('用户名:'+userName)
+	_print('密码:'+userPass)
+	while True:
+		_print('是否确认或退出(Y/N/exit)')
+		uIn = raw_input()
+		if uIn.upper().strip() == YES:
+			rtn = doRegister(userName, userPass)
+			break
+		elif uIn.upper().strip() == No:
+			rtn = register()
+			break
+		elif uIn.upper().strip() == 'EXIT':
+			rtn['code'] = -1
+			break
+	return rtn
+
+def doRegister(userName,userPass):
+
+	#TODO 注册功能
+	rtn = {'userName':None,'userPass':None,'code':-1}
+	rtn['code'] = 1
+	rtn['userName'] = userName
+	rtn['userPass'] = userPass
+	return rtn;
+
+main()
+
+
+
+
+
---
+++ test009.py
@@ -0,0 +1,43 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-  
+# 函数的参数
+def _print(msg=''):
+	print _toUtf8(msg)
+
+def _toUtf8(msg):
+	return '%s'%(msg.decode('utf-8'))
+
+_print('默认参数:在函数定义的时候可以设置参数的默认值(默认值最好设置不可变类型),设置了默认值的参数在函数调用的时候可以不填值')
+def pingfang(num,mi=2):
+	if num == None:
+		raise RuntimeError('args num is null')
+	rtn = num
+	for x in xrange(mi-1):
+		rtn = rtn * num
+	return rtn
+print pingfang(2)
+print pingfang(2,3)
+
+_print('可变参数,在参数前面添加一个"*"号可以使该参数成为可变参数,可变参数会自动把参数组装成一个tuple,可变参数可以不填')
+def concat(*strs):
+	rtn = ''
+	for s in strs:
+		rtn = rtn + s
+	return rtn
+print concat('s','bin')
+def concat1(one,*strs):
+	rtn = one
+	for s in strs:
+		rtn = rtn + s
+	return rtn
+print concat('a')
+
+_print('关键字参数,关键字参数允许你传入0个或任意个含参数名的参数,这些关键字参数在函数内部自动组装为一个dict')
+
+def person(name,age,**other):
+	print 'name:',name,'age:',age,'other:',other
+	
+other = {'city':'beijing'}
+person('he', 19,**other)
+
+
