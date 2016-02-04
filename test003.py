75565f4268ca152caff7f09b1103743629ecdce7
陈锐 <chenrui_accp@163.com>
2016-02-04 14:50:38 +0800

    1

--- test003.py
+++ test003.py
@@ -4,3 +4,4 @@
 print u'测试中文'
 print u'format string'
 print u'格式化字符串:第一个参数(string类型)[%s],第二个参数(int)[%d],第三个参数(float)[%f],第四个参数(16进制)[%x],第五个参数(boolean)[%d]' %(u'第一参数',111,11.1,0xa2,True or False)
+
