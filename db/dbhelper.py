08b60fd2d091978f75574511658504e0b564fefb
陈锐 <chenrui_accp@163.com>
2016-03-11 11:27:22 +0800

    添加查询

--- db/dbhelper.py
+++ db/dbhelper.py
@@ -19,8 +19,8 @@ class DbHelper(object):
 		self.conn.commit()
 		self.conn.close()
 
-	def queryAll(self,tableName):
-		sql = "select * from %s "%(tableName)
+	def executeQuery(self,sql):
+		print sql
 		cursor = self.getConn()
 		cursor.execute(sql)
 		values = cursor.fetchall()
@@ -28,9 +28,34 @@ class DbHelper(object):
 		return values
 
 
+	def queryAll(self,tableName):
+		sql = "select * from %s "%(tableName)
+		values = self.executeQuery(sql)
+		return values
+
+	def query(self,fieldsName,tableName):
+		sql = "select "
+		sql = sql + self.generateFieldsSql(fieldsName)
+		sql = sql + "from %s "%(tableName)
+		values = self.executeQuery(sql)
+		return values
+
+	def generateFieldsSql(self,fieldsName):
+		fieldSql = "";
+		for i,field in enumerate(fieldsName):
+			fieldSql = fieldSql + field
+			if i < len(fieldsName) - 1:
+				fieldSql = fieldSql + ","
+			else :
+				fieldSql = fieldSql + " "
+		return fieldSql
+
+
+
 if __name__  == '__main__':
 	cf = DbHelper("test.db")
-	values = cf.queryAll('TEST_TABLE')
-	for id,name in values:
-		print id + "-->" + name
+	#values = cf.queryAll('TEST_TABLE')
+	values = cf.query(['id'],'TEST_TABLE')
+	for id in values:
+		print id 
 
